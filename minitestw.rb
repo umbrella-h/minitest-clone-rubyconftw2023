18105290b4e102dfe8c1704be911864eb43521fa
KasaHNO3 <umbrella.hsiao@gmail.com>
2024-07-01 01:28:19 +0800

    feat: recognize method names started with 'test_'

--- minitestw.rb
+++ minitestw.rb
@@ -1,3 +1,19 @@
-require 'minitest'
-require 'minitest/autorun'
-Minitestw = Minitest
+module Minitestw
+  class Test
+    def self.inherited(subclass)
+      subclass.class_eval do
+        def self.method_added(name)
+          # p self
+          @test_methods ||= []
+          @test_methods << name if name.to_s.match(/^test_/)
+          super
+        end
+
+        at_exit do
+          # p self # if forgeting `self.inherited`, the output will be 'Minitestw::Test' and `nil`
+          p @test_methods
+        end
+      end
+    end
+  end
+end
