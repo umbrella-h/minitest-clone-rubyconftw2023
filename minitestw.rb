9c07dc28fdd5eef94ac5264470ee4d434f475bca
KasaHNO3 <umbrella.hsiao@gmail.com>
2024-07-01 01:30:36 +0800

    feat: execute the test methods

--- minitestw.rb
+++ minitestw.rb
@@ -3,15 +3,17 @@ module Minitestw
     def self.inherited(subclass)
       subclass.class_eval do
         def self.method_added(name)
-          # p self
           @test_methods ||= []
           @test_methods << name if name.to_s.match(/^test_/)
           super
         end
 
         at_exit do
-          # p self # if forgeting `self.inherited`, the output will be 'Minitestw::Test' and `nil`
-          p @test_methods
+          instance = new
+          instance.setup
+          @test_methods.each do |test_method|
+            instance.__send__(test_method)
+          end
         end
       end
     end
