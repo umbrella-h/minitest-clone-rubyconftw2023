a66f9281aadc5cbe52e8cf96e2a0c2e6eab1ffce
KasaHNO3 <umbrella.hsiao@gmail.com>
2024-07-01 01:53:05 +0800

    output numbers of runs, assertions, and failures.

--- minitestw.rb
+++ minitestw.rb
@@ -1,5 +1,24 @@
 module Minitestw
   class Test
+    attr_reader :failures, :assertion_size
+
+    def initialize
+      @failures = []
+      @assertion_size = 0
+    end
+
+    def assert(statement)
+      @assertion_size += 1
+      # TODO: Failure class
+      @failures << Object.new unless statement
+    end
+
+    def assert_equal(expected, actual)
+      @assertion_size += 1
+      # TODO: Failure class
+      @failures << Object.new unless expected == actual
+    end
+
     def self.inherited(subclass)
       subclass.class_eval do
         def self.method_added(name)
@@ -14,6 +33,7 @@ module Minitestw
           @test_methods.each do |test_method|
             instance.__send__(test_method)
           end
+          puts "#{@test_methods.size} runs, #{instance.assertion_size} assertions, #{instance.failures.size} failures"
         end
       end
     end
